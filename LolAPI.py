38cd6d02b4ce9a8210527e538f347a3a46e16d24
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-16 20:07:24 -0300

    Projeto em andamento.

--- LolAPI.py
+++ LolAPI.py
@@ -1,12 +1,22 @@
+from urllib import response
 from requests import get
 
-LOL_KEY = "?api_key=RGAPI-89613424-0bda-4b09-9d27-faf8b9b040a0"
+LOL_KEY = "?api_key=RGAPI-ccb73e02-e79a-4064-aabf-a2496c779776"
 LOL_URL = "https://br1.api.riotgames.com"
+LOL_NAME = input("Digite o nome de Invocador: ")
 
 
 def get_links():
-    response = get(LOL_URL+--+LOL_KEY).json()
+    response = get(LOL_URL+"/lol/summoner/v4/summoners/by-name/" +
+                   LOL_NAME+LOL_KEY).json()
     return response
 
 
-print(get_links())
+def ranked():
+    lol_id = get_links()["id"]
+    response = get(
+        LOL_URL+"/lol/league/v4/entries/by-summoner/"+lol_id+LOL_KEY).json()
+    return response
+
+
+print(ranked())
