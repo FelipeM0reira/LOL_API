bab468db079e0f8fb5116602b5755ce1758d2877
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-17 21:43:29 -0300

    Continuação do projeto!

--- LolAPI.py
+++ LolAPI.py
@@ -1,4 +1,3 @@
-from urllib import response
 from requests import get
 
 KEY = "RGAPI-a0b4f9c2-ce38-4a19-9471-e6727dce4197"
@@ -21,5 +20,14 @@ def ranked():
 
 
 for rank in ranked():
-    rankSolo = rank["tier"]
-    print(rank)
+    elo = rank["tier"]
+    rankeada = rank["queueType"]
+    tier = rank["rank"]
+    pontos = rank["leaguePoints"]
+    vit = rank["wins"]
+    derrotas = rank["losses"]
+    txvit = vit*100/(vit+derrotas)
+
+
+print("Tipo de rankeada: {}, Rank: {}, Tier: {}, Pontos: {}, Vitorias: {}, Derrotas: {}, Porgentagem de vitoria: {}%.".format(
+    rankeada, elo, tier, pontos, vit, derrotas, txvit))
